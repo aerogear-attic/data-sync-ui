74a53a31aba753223760f2258e441191949a2bf9
Jose Miguel Gallas <josemigallas@gmail.com>
2018-07-03 12:42:48 +0200

    fixes lint

--- test/server/tests.js
+++ test/server/tests.js
@@ -1,5 +1,5 @@
-const App = require("../server")
-    , request = require("supertest");
+import { agent } from "supertest";
+import { run, stop } from "../../server/server";
 
 describe("Basic", () => {
     let user = null;
--- webpack.config.js
+++ webpack.config.js
@@ -18,28 +18,3 @@ module.exports = {
         ]
     }
 };
-
-/*
-const path = require("path");
-
-module.exports = {
-    entry: {
-        "public/js/bundle": "./ui/ui.js",
-        "dist/server": "./server/index.js"
-    },
-    output: {
-        path: path.resolve(__dirname),
-        filename: "[name].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: { loader: "babel-loader" }
-            }
-        ]
-    },
-    node: { fs: "empty", net: "empty" }
-};
-*/
