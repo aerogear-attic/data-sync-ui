3ef8543768ddfde3986011ef7656486123606645
Peter Braun <pbraun@redhat.com>
2018-07-06 11:10:32 +0200

    eslint fixes

--- server/health/healthchecks.js
+++ server/health/healthchecks.js
@@ -1,21 +1,17 @@
-import {addCriticalTest, runTests} from "fh-health";
-import { database } from "../models"
+import { addCriticalTest, runTests } from "fh-health";
+import { database } from "../models";
 
 addCriticalTest("Database connectivity", cb => {
     database.authenticate()
-        .then(() => {
-            return cb(null, "Database connectivity check successful");
-        })
-        .catch(err => {
-            return cb(err);
-        });
+        .then(() => cb(null, "Database connectivity check successful"))
+        .catch(err => cb(err));
 });
 
 export function addHealthEndpoint(App) {
     App.get("/healthz", (req, res) => {
         runTests((err, data) => {
             if (err) {
-                return res.sendStatus(500)
+                return res.sendStatus(500);
             }
 
             return res.json(JSON.parse(data));
