0cba1acbd6cea2bba9c5696c47ac2ca608169de6
Peter Braun <pbraun@redhat.com>
2018-07-06 09:11:17 +0200

    eslint fixes

--- server/server.js
+++ server/server.js
@@ -18,9 +18,7 @@ App.use(express.static(join(__dirname, "../public")));
 App.get("/", (req, res) => res.sendFile(join(__dirname, "../public/index.html")));
 
 // Indicates that the sync ui is up and healthy
-App.get("/healthz", (req, res) => {
-    return res.sendStatus(200);
-});
+App.get("/healthz", (req, res) => res.sendStatus(200));
 
 setupGraphQLServer(App);
 
