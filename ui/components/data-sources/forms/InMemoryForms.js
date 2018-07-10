50b2577bc76ae4b012dba51d661fc678ed6ef492
Jose Miguel Gallas <josemigallas@gmail.com>
2018-07-10 17:05:08 +0200

    removes collection form from inmemory forms

--- ui/components/data-sources/forms/InMemoryForms.js
+++ ui/components/data-sources/forms/InMemoryForms.js
@@ -1,21 +1,8 @@
 import React from "react";
-import {
-    FormControl, FormGroup, Col
-} from "patternfly-react";
 
-const InMemoryForms = ({ collection = "", onCollectionChange }) => (
+const InMemoryForms = () => (
     <React.Fragment>
-        {/* Collection */}
-        <FormGroup controlId="collection">
-            <Col sm={3}>Collection</Col>
-            <Col sm={9}>
-                <FormControl
-                    type="text"
-                    value={collection}
-                    onChange={e => onCollectionChange(e.target.value)}
-                />
-            </Col>
-        </FormGroup>
+        {}
     </React.Fragment>
 );
 
