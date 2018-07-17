73bfdc59409fa9a9cd63ba3b8301ac746883cff1
Jose Miguel Gallas <josemigallas@gmail.com>
2018-07-17 11:20:10 +0200

    small refactor

--- ui/components/data-sources/DataSourcesList.js
+++ ui/components/data-sources/DataSourcesList.js
@@ -1,34 +1,39 @@
 import React from "react";
 import { Query } from "react-apollo";
-import { ListView } from "patternfly-react";
+import { ListView, Spinner } from "patternfly-react";
 import { EmptyList } from "./EmptyList";
 import GetDataSources from "../../graphql/GetDataSources.graphql";
 import { DataSourcesListItem } from "./DataSourcesListItem";
 
-const DataSourcesList = ({ filter, onCreate, onEditDataSource, onDeleteDataSource }) => (
-    <Query query={GetDataSources} variables={filter}>
-        {({ loading, error, data }) => {
-            if (loading) {
-                return "Loading...";
-            }
-            if (error) {
-                return error.message;
-            }
-            const items = data.dataSources.map(item => (
-                <DataSourcesListItem
-                    item={item}
-                    key={item.id}
-                    onEditDataSource={onEditDataSource}
-                    onDeleteDataSource={onDeleteDataSource}
-                />
-            ));
+const DataSourcesList = ({ filter, onCreate, onEditDataSource, onDeleteDataSource }) => {
+    const renderItems = data => data.dataSources.map(item => (
+        <DataSourcesListItem
+            item={item}
+            key={item.id}
+            onEditDataSource={onEditDataSource}
+            onDeleteDataSource={onDeleteDataSource}
+        />
+    ));
 
-            if (items.length > 0) {
-                return (<ListView>{items}</ListView>);
-            }
-            return <EmptyList createDataSource={onCreate} />;
-        }}
-    </Query>
-);
+    return (
+        <Query query={GetDataSources} variables={filter}>
+            {({ loading, error, data }) => {
+                if (loading) {
+                    return "Loading...";
+                }
+
+                if (error) {
+                    return error.message;
+                }
+
+                if (data.dataSources.length) {
+                    return <ListView>{renderItems(data)}</ListView>;
+                }
+
+                return <EmptyList createDataSource={onCreate} />;
+            }}
+        </Query>
+    );
+};
 
 export { DataSourcesList };
