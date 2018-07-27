77336115969365aa0535daa6d515b186b5b73ec5
Peter Braun <pbraun@redhat.com>
2018-07-27 09:10:50 +0200

    template strings for formatting

--- ui/components/common/GraphQLFormatters.js
+++ ui/components/common/GraphQLFormatters.js
@@ -2,13 +2,10 @@ const formatType = type => {
     let result = "";
     switch (type.kind) {
         case "LIST":
-            result += "[";
-            result += formatType(type.ofType);
-            result += "]";
+            result += `[${formatType(type.ofType)}]`;
             break;
         case "NON_NULL":
-            result += formatType(type.ofType);
-            result += "!";
+            result += `${formatType(type.ofType)}!`;
             break;
         default:
             result += type.name;
