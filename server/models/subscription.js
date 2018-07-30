05af21fd032826978f4e5997e1e9d41c6e5d25db
Pavel Šturc <psturc@redhat.com>
2018-07-30 14:26:25 +0200

    fix: eslint

--- server/models/subscription.js
+++ server/models/subscription.js
@@ -1,19 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-  const Subscription = sequelize.define('Subscription', {
-    type: DataTypes.ENUM('Subscription'),
-    field: DataTypes.STRING,
-    topic: DataTypes.STRING,
-    filter: DataTypes.JSON
-  })
+    const Subscription = sequelize.define("Subscription", {
+        type: DataTypes.ENUM("Subscription"),
+        field: DataTypes.STRING,
+        topic: DataTypes.STRING,
+        filter: DataTypes.JSON
+    });
 
-  Subscription.associate = (models) => {
-    models.Subscription.belongsTo(models.GraphQLSchema, {
-      onDelete: 'CASCADE',
-      foreignKey: {
-        allowNull: false
-      }
-    })
-  }
+    Subscription.associate = models => {
+        models.Subscription.belongsTo(models.GraphQLSchema, {
+            onDelete: "CASCADE",
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
 
-  return Subscription
-}
+    return Subscription;
+};
