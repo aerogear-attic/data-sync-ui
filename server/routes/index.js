4b34b53e0b9221d172169698cc8a3e6e5c888e22
Jose Miguel Gallas <josemigallas@gmail.com>
2018-07-31 16:36:16 +0200

    merge all server tests

---
+++ server/routes/index.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const { join } = require("path");
+const { runHealthChecks } = require("../health");
+const { log } = require("../logger");
+const { compileSchemaString } = require("../gql/helper");
+const { schema } = require("../models");
+const setupGraphQLServer = require("../gql");
+
+router.get("/", (req, res) => res.sendFile(join(__dirname, "../public/index.html")));
+
+router.get("/healthz", (req, res) => {
+    runHealthChecks().then(result => {
+        res.status(result.ok ? 200 : 503);
+        return res.json(result);
+    }).catch(err => {
+        log.error(err);
+        return res.sendStatus(500);
+    });
+});
+
+router.get("/schema/:schemaId", async (req, res) => {
+    try {
+        const s = await schema.findById(req.params.schemaId);
+        if (s) {
+            const compiled = await compileSchemaString(s.schema);
+            const filename = `schema_${s.name}.json`;
+            // tell the browser to handle this as a file download
+            res.setHeader("Content-disposition", `attachment; filename=${filename}`);
+            res.setHeader("Content-type", "text/json");
+            res.status(200).send(JSON.stringify(compiled, null, 2));
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (err) {
+        log.error(err);
+        res.sendStatus(500);
+    }
+});
+
+setupGraphQLServer(router);
+
+// TODO: add graphql route here
+
+module.exports = router;
--- test/server/server.test.js
+++ test/server/server.test.js
@@ -1,5 +1,20 @@
+const { graphql } = require("graphql");
 const { agent } = require("supertest");
+const { Schema, root } = require("../../server/gql/schema");
 const { run, stop } = require("../../server/server");
+const { reset, sync } = require("../../server/models");
+const {
+    CREATE_DATA_SOURCE,
+    GET_DATA_SOURCES,
+    GET_DATA_SOURCE,
+    DELETE_DATA_SOURCE,
+    UPDATE_DATA_SOURCE,
+    GET_SCHEMAS,
+    GET_SCHEMA,
+    UPDATE_SCHEMA,
+    GET_RESOLVERS,
+    UPSERT_RESOLVER
+} = require("./queries");
 
 let user = null;
 
@@ -14,12 +29,204 @@ afterAll(done => {
     stop(() => done());
 });
 
-it("should serve the ui", async () => {
-    const res = await user.get("/");
-    expect(res.status).toEqual(200);
+afterEach(async () => {
+    await reset();
+    await sync();
 });
 
-it("should expose a health endpoint", async () => {
-    const res = await user.get("/healthz");
-    expect(res.status).toEqual(200);
+describe("Routes", () => {
+    it("should serve the ui", async () => {
+        const res = await user.get("/");
+        expect(res.status).toEqual(200);
+    });
+
+    it("should expose a health endpoint", async () => {
+        const res = await user.get("/healthz");
+        expect(res.status).toEqual(200);
+    });
+
+    it("should expose a graphql console", async () => {
+        const res = await user.get("/graphql");
+        expect(res.status).toEqual(200);
+    });
+});
+
+describe("Database", () => {
+    const dataSource = {
+        name: "test",
+        type: "Postgres",
+        config: { timestamps: true },
+        resolvers: []
+    };
+
+    const resolver = {
+        type: "type",
+        field: "field",
+        requestMapping: "",
+        responseMapping: ""
+    };
+
+    describe("DataSources", () => {
+        it("should not have any data sources by default", async () => {
+            const fetch = await graphql(Schema, GET_DATA_SOURCES, root);
+            expect(fetch.data.dataSources).toHaveLength(0);
+        });
+
+        it("should create a new data source", async () => {
+            await graphql(Schema, CREATE_DATA_SOURCE, root, null, dataSource);
+
+            const fetch = await graphql(Schema, GET_DATA_SOURCES, root);
+            expect(fetch.data.dataSources).toHaveLength(1);
+            expect(fetch.data.dataSources[0]).toEqual({ id: 1, ...dataSource });
+        });
+
+        it("should delete an existing data source", async () => {
+            await graphql(Schema, CREATE_DATA_SOURCE, root, null, dataSource);
+
+            let fetch = await graphql(Schema, GET_DATA_SOURCES, root);
+            expect(fetch.data.dataSources).toHaveLength(1);
+            expect(fetch.data.dataSources[0]).toEqual({ id: 1, ...dataSource });
+
+            await graphql(Schema, DELETE_DATA_SOURCE, root, null, { id: 1 });
+
+            fetch = await graphql(Schema, GET_DATA_SOURCES, root);
+            expect(fetch.data.dataSources).toHaveLength(0);
+        });
+
+        it("should get one data source by its id", async () => {
+            await graphql(Schema, CREATE_DATA_SOURCE, root, null, dataSource);
+
+            const result = await graphql(Schema, GET_DATA_SOURCE, root, null, { id: 1 });
+            expect(result.errors).toBeUndefined();
+            expect(result.data.getOneDataSource).toEqual({ id: 1, ...dataSource });
+        });
+
+        it("should edit an existing data source", async () => {
+            const updatedDataSource = {
+                id: 1,
+                name: "updated!",
+                type: "InMemory",
+                config: { timestamps: false },
+                resolvers: []
+            };
+
+            await graphql(Schema, CREATE_DATA_SOURCE, root, null, dataSource);
+
+            let fetch = await graphql(Schema, GET_DATA_SOURCE, root, null, { id: 1 });
+            expect(fetch.data.getOneDataSource).toEqual({ id: 1, ...dataSource });
+
+            await graphql(Schema, UPDATE_DATA_SOURCE, root, null, updatedDataSource);
+
+            fetch = await graphql(Schema, GET_DATA_SOURCE, root, null, { id: 1 });
+            expect(fetch.data.getOneDataSource).toEqual({ id: 1, ...updatedDataSource });
+        });
+    });
+
+    describe("Schemas", () => {
+        it("should not have any schemas by default", async () => {
+            const fetch = await graphql(Schema, GET_SCHEMAS, root);
+            expect(fetch.data.schemas).toHaveLength(0);
+        });
+
+        it("should create an empty schema when fetching a non existent one", async () => {
+            let fetch = await graphql(Schema, GET_SCHEMAS, root);
+            expect(fetch.data.schemas).toHaveLength(0);
+
+            fetch = await graphql(Schema, GET_SCHEMA, root, null, { name: "test" });
+            expect(fetch.data.getSchema).toBeDefined();
+            expect(fetch.data.getSchema.name).toEqual("test");
+            expect(fetch.data.getSchema.schema).toEqual("# Add your schema here");
+            expect(fetch.data.getSchema.compiled).toEqual(JSON.stringify(""));
+
+            fetch = await graphql(Schema, GET_SCHEMAS, root);
+            expect(fetch.data.schemas).toHaveLength(1);
+        });
+
+        it("should update a schema", async () => {
+            const schema = `
+            type Note {
+                id: Int,
+                name: String
+            }
+    
+            type Query {
+                notes: [Note]
+            }
+        `;
+
+            let fetch = await graphql(Schema, GET_SCHEMA, root, null, { name: "test" });
+            expect(fetch.data.getSchema.name).toEqual("test");
+            expect(fetch.data.getSchema.schema).toEqual("# Add your schema here");
+
+            await graphql(Schema, UPDATE_SCHEMA, root, null, { id: 1, schema });
+
+            fetch = await graphql(Schema, GET_SCHEMA, root, null, { name: "test" });
+            expect(fetch.data.getSchema.name).toEqual("test");
+            expect(fetch.data.getSchema.schema).toEqual(schema);
+        });
+    });
+
+    describe("Resolvers", () => {
+        it("should not have any resolvers by default", async () => {
+            const fetch = await graphql(Schema, GET_RESOLVERS, root, null, { schemaId: 1, type: "type" });
+            expect(fetch.data.resolvers).toHaveLength(0);
+        });
+
+        it("should throw an error if creating a resolver without schema", async () => {
+            await graphql(Schema, CREATE_DATA_SOURCE, root, null, dataSource);
+
+            let fetch = await graphql(Schema, GET_DATA_SOURCES, root);
+            expect(fetch.data.dataSources).toHaveLength(1);
+
+            fetch = await graphql(Schema, GET_SCHEMAS, root);
+            expect(fetch.data.schemas).toHaveLength(0);
+
+            const create = await graphql(Schema, UPSERT_RESOLVER, root, null, {
+                schemaId: 1, dataSourceId: 1, ...resolver
+            });
+            expect(create.errors).toHaveLength(1);
+        });
+
+        it("should throw an error if creating a resolvers without a data source", async () => {
+            await graphql(Schema, GET_SCHEMA, root, null, { name: "test" });
+
+            let fetch = await graphql(Schema, GET_SCHEMAS, root);
+            expect(fetch.data.schemas).toHaveLength(1);
+
+            fetch = await graphql(Schema, GET_DATA_SOURCES, root);
+            expect(fetch.data.dataSources).toHaveLength(0);
+
+            const create = await graphql(Schema, UPSERT_RESOLVER, root, null, {
+                schemaId: 1, dataSourceId: 1, ...resolver
+            });
+            expect(create.errors).toHaveLength(1);
+        });
+
+        it("should create a resolver and attach it to a data source", async () => {
+            await graphql(Schema, GET_SCHEMA, root, null, { name: "test" });
+            await graphql(Schema, CREATE_DATA_SOURCE, root, null, dataSource);
+
+            await graphql(Schema, UPSERT_RESOLVER, root, null, {
+                schemaId: 1, dataSourceId: 1, ...resolver
+            });
+
+            const fetch = await graphql(Schema, GET_DATA_SOURCE, root, null, { id: 1 });
+            expect(fetch.data.getOneDataSource.resolvers).toHaveLength(1);
+            expect(fetch.data.getOneDataSource.resolvers[0]).toEqual({ id: 1, ...resolver });
+        });
+
+        it("should create a resolver", async () => {
+            await graphql(Schema, GET_SCHEMA, root, null, { name: "test" });
+            await graphql(Schema, CREATE_DATA_SOURCE, root, null, dataSource);
+
+            await graphql(Schema, UPSERT_RESOLVER, root, null, {
+                schemaId: 1, dataSourceId: 1, ...resolver
+            });
+
+            const fetch = await graphql(Schema, GET_RESOLVERS, root, null, {
+                schemaId: 1, type: resolver.type
+            });
+            expect(fetch.data.resolvers).toHaveLength(1);
+        });
+    });
 });
