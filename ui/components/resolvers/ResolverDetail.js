b6b2ff54d1693fa46512f4c64793d3f5addc2175
Jose Miguel Gallas <josemigallas@gmail.com>
2018-08-02 14:04:09 +0200

    adds button and inner scroll for forms only

--- ui/components/resolvers/ResolverDetail.js
+++ ui/components/resolvers/ResolverDetail.js
@@ -77,50 +77,68 @@ class ResolverDetail extends Component {
         return (
             <React.Fragment>
                 <h3 className={styles.detailHeader}>Edit {resolver.type}.{resolver.field}</h3>
-                <h3 className={styles.detailSubHeader}>Resolver
-                    <span className={styles.learnMore}>
-                        <a href="https://www.google.es">Learn More <span className="fa fa-external-link" /></a>
-                    </span>
-                </h3>
+                <div className={styles.detailFormsContainer}>
+                    <h3 className={styles.detailFormsHeader}>Resolver
+                        <span className={styles.learnMore}>
+                            <a href="https://www.google.es">Learn More <span className="fa fa-external-link" /></a>
+                        </span>
+                    </h3>
 
-                <Form horizontal className={styles.formContainer}>
-                    <FormGroup controlId="dataSource" className={styles.detailFormGroup}>
-                        <DataSourcesDropDown
-                            selected={dataSource}
-                            onDataSourceSelect={ds => this.setState({ dataSource: ds })}
-                        />
-                    </FormGroup>
+                    <Form horizontal className={styles.formContainer}>
+                        <FormGroup controlId="dataSource" className={styles.detailFormGroup}>
+                            <DataSourcesDropDown
+                                selected={dataSource}
+                                onDataSourceSelect={ds => this.setState({ dataSource: ds })}
+                            />
+                        </FormGroup>
 
-                    <FormGroup controlId="responseMapping" className={styles.detailFormGroup}>
-                        <ResponseMappingTemplateDropDown
-                            value={responseMapping}
-                            onSelect={item => this.setState({ responseMapping: item })}
-                        />
-                        <Col sm={12}>
-                            <div className={styles.detailCodeEditor}>
-                                <CodeEditor
-                                    value={responseMappingTemplate}
-                                    onChange={t => this.setState({ responseMappingTemplate: t })}
-                                />
-                            </div>
-                        </Col>
-                    </FormGroup>
+                        <FormGroup controlId="responseMapping" className={styles.detailFormGroup}>
+                            <ResponseMappingTemplateDropDown
+                                value={responseMapping}
+                                onSelect={item => this.setState({ responseMapping: item })}
+                            />
+                            <Col sm={12}>
+                                <div className={styles.detailCodeEditor}>
+                                    <CodeEditor
+                                        value={responseMappingTemplate}
+                                        onChange={t => this.setState({ responseMappingTemplate: t })}
+                                    />
+                                </div>
+                            </Col>
+                        </FormGroup>
 
-                    <FormGroup controlId="requestMapping" className={styles.detailFormGroup}>
-                        <RequestMappingTemplateDropDown
-                            value={requestMapping}
-                            onSelect={item => this.setState({ requestMapping: item })}
-                        />
-                        <Col sm={12}>
-                            <div className={styles.detailCodeEditor}>
-                                <CodeEditor
-                                    value={requestMappingTemplate}
-                                    onChange={t => this.setState({ requestMappingTemplate: t })}
-                                />
-                            </div>
-                        </Col>
-                    </FormGroup>
-                </Form>
+                        <FormGroup controlId="requestMapping" className={styles.detailFormGroup}>
+                            <RequestMappingTemplateDropDown
+                                value={requestMapping}
+                                onSelect={item => this.setState({ requestMapping: item })}
+                            />
+                            <Col sm={12}>
+                                <div className={styles.detailCodeEditor}>
+                                    <CodeEditor
+                                        value={requestMappingTemplate}
+                                        onChange={t => this.setState({ requestMappingTemplate: t })}
+                                    />
+                                </div>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </div>
+
+                <div className={styles.detailButtonFooter}>
+                    <Button
+                        className={styles.buttonSave}
+                        bsStyle="primary"
+                        onClick={() => this.save()}
+                    >
+                        Save
+                    </Button>
+                    <Button
+                        bsStyle="default"
+                        onClick={() => this.cancel()}
+                    >
+                        Cancel
+                    </Button>
+                </div>
 
             </React.Fragment>
         );
