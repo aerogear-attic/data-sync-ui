c623180c16c9c16915cab20d0cd695121ab9a425
Jose Miguel Gallas <josemigallas@gmail.com>
2018-08-06 14:50:12 +0200

    adds dummy verification for hooks

--- ui/components/resolvers/HookFormGroup.js
+++ ui/components/resolvers/HookFormGroup.js
@@ -3,18 +3,55 @@ import {
     Col,
     FormControl,
     InputGroup,
-    Button
+    Button,
+    Icon
 } from "patternfly-react";
+
+import { verifyButton, verifyButtonLabel } from "./HookFormGroup.css";
+
+const INITIAL_STATE = {
+    verifyingUrl: false,
+    verificationResult: null
+};
 
 class HookFormGroup extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = INITIAL_STATE;
+    }
+
     testHook() {
+        // TODO: implement verification with server
         const { url = "" } = this.props;
-        console.log(`Test url: ${url}`);
+        console.log("Verifying url...", url);
+
+        this.setState({ verifyingUrl: true, verificationResult: null });
+
+        setTimeout(() => this.setState({ verifyingUrl: false, verificationResult: "error" }), 1000);
+    }
+
+    renderVerificationIcon() {
+        const { verifyingUrl, verificationResult } = this.state;
+
+        if (verifyingUrl) {
+            return <Icon name="spinner" spin />;
+        }
+
+        if (verificationResult === "success") {
+            return <Icon type="pf" name="ok" />;
+        }
+
+        if (verificationResult === "error") {
+            return <Icon type="pf" name="error-circle-o" />;
+        }
+
+        return null;
     }
 
     render() {
         const { url = "", label, onChange } = this.props;
+        const { verifyingUrl } = this.state;
 
         return (
             <React.Fragment>
@@ -27,8 +64,9 @@ class HookFormGroup extends Component {
                             onChange={ev => onChange(ev.currentTarget.value)}
                         />
                         <InputGroup.Button>
-                            <Button onClick={() => this.testHook()}>
-                            Verify URL
+                            <Button onClick={() => this.testHook()} className={verifyButton}>
+                                <span className={verifyButtonLabel}>{verifyingUrl ? "Verifying..." : "Verify URL"}</span>
+                                {this.renderVerificationIcon()}
                             </Button>
                         </InputGroup.Button>
                     </InputGroup>
@@ -36,6 +74,7 @@ class HookFormGroup extends Component {
             </React.Fragment>
         );
     }
+
 }
 
 export { HookFormGroup };
