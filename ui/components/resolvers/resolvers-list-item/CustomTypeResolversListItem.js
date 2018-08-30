25f4965f77f082416bd836e06e060e0597061864
Jose Miguel Gallas <josemigallas@gmail.com>
2018-08-30 10:49:25 +0200

    adds counter to custom types

--- ui/components/resolvers/resolvers-list-item/CustomTypeResolversListItem.js
+++ ui/components/resolvers/resolvers-list-item/CustomTypeResolversListItem.js
@@ -8,24 +8,19 @@ import {
     Col
 } from "patternfly-react";
 import { CustomTypeArgs } from "./CustomTypeArgs";
+import { ResolversCount } from "../../common";
 
 import GetResolvers from "../../../graphql/GetResolvers.graphql";
 
 import {
     headingDescription,
+    headingDescriptionContainer,
     resolversHeading,
     resolverFieldRow
 } from "../ResolversListItem.css";
 
 const CustomTypeResolversListItem = ({ schemaId, type, item, onClick }) => {
     const { fields, name } = item;
-    const fieldsText = (
-        <span className={headingDescription}>
-            <span style={{ fontWeight: "600" }}>{fields.length}</span>
-            <span style={{ fontWeight: "300" }}>{fields.length !== 1 ? " Arguments" : " Argument"}</span>
-        </span>
-    );
-
     return (
         <Query key={name} query={GetResolvers} variables={{ schemaId, type: name }}>
             {({ loading, error, data }) => {
@@ -35,6 +30,21 @@ const CustomTypeResolversListItem = ({ schemaId, type, item, onClick }) => {
                 if (error) {
                     return error.message;
                 }
+
+                const definedResolvers = fields.reduce((prev, curr) => {
+                    const resolver = data.resolvers.find(r => r.field === curr.name);
+                    return resolver ? prev + 1 : prev;
+                }, 0);
+
+                const fieldsText = (
+                    <div className={headingDescriptionContainer}>
+                        <span className={headingDescription}>
+                            <span style={{ fontWeight: "600" }}>{fields.length}</span>
+                            <span style={{ fontWeight: "300" }}>{fields.length !== 1 ? " Arguments" : " Argument"}</span>
+                        </span>
+                        <ResolversCount total={fields.length} defined={definedResolvers} />
+                    </div>
+                );
 
                 return (
                     <ListViewItem
