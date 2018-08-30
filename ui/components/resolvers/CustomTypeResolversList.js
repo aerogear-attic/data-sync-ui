a94100712eefd7549dec0c4089eed9d51606f52b
Jose Miguel Gallas <josemigallas@gmail.com>
2018-08-30 11:09:07 +0200

    fixes custom type header

--- ui/components/resolvers/CustomTypeResolversList.js
+++ ui/components/resolvers/CustomTypeResolversList.js
@@ -3,6 +3,7 @@ import { ListView } from "patternfly-react";
 import { CustomTypeResolversListItem } from "./resolvers-list-item";
 
 import styles from "./ResolversListItem.css";
+import { learnMore } from "../common/common.css";
 
 /**
  * A CustomTypeItem is used to render a user defined GraphQL type in the resolver tab's
@@ -10,10 +11,17 @@ import styles from "./ResolversListItem.css";
  */
 const CustomTypeResolversList = props => {
     const { items, text, schemaId, kind, onClick } = props;
-    const { resolversContent, resolversHeader, resolversList } = styles;
+    const { resolversContent, resolversHeader, resolversHeaderName, resolversList } = styles;
     return (
         <div className={resolversContent}>
-            <div className={resolversHeader}>{text}</div>
+            <div className={resolversHeader}>
+                <div className={resolversHeaderName}>
+                    <span style={{ marginRight: "12px" }}>{text}</span>
+                    <span className={learnMore}>
+                        <a href="https://www.google.es">Learn More <span className="fa fa-external-link" /></a>
+                    </span>
+                </div>
+            </div>
             <ListView className={resolversList}>
                 {items.map(item => (
                     <CustomTypeResolversListItem
