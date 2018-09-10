667893c2b49334a94bf64fb2ad25aa5c8fce92a7
Summers Pittman <secondsun@gmail.com>
2018-09-10 07:28:28 -0400

    formatting

--- ui/components/queries/QueriesContainer.js
+++ ui/components/queries/QueriesContainer.js
@@ -2,9 +2,15 @@ import React from "react";
 import { Provider } from "react-redux";
 import { Playground, store } from "graphql-playground-react";
 
+const settings = {
+    "editor.theme": "light",
+    "editor.cursorShape": "line"
+};
+
 const QueriesContainer = () => (
     <Provider store={store}>
-        <Playground endpoint="http://localhost:8000/graphql" settings={{ "editor.theme": "light", "editor.cursorShape": "line" }} />
-    </Provider>);
+        <Playground endpoint="http://localhost:8000/graphql" settings={settings} />
+    </Provider>
+);
 
 export { QueriesContainer };
